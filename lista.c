c2397faad4934c9a392fcf2d1ff4e7550bad5e8d
Beatriz Andrade <beatriz.anddradesilva@gmail.com>
2024-06-23 10:26:25 -0300

    committed lista.c committed lista.h committed main.c4

--- lista.c
+++ lista.c
@@ -1,1 +1,46 @@
 #include <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "lista.h"
+
+void list_info(List * L) {
+    printf("Tamanho maximo: %d\n", L->size);
+    printf("Quantidade de elementos: %d\n", L->n);
+    printf("Ponteiro do vetor: %p\n", L->e);
+    list_print(L);
+}
+
+List * list_new( int size ) {
+    List * L = NULL;
+    L = (List *)calloc(1, sizeof(List));
+    L->n = 0;
+    L->e = (int*)calloc(size, sizeof(int) );
+    L->size = size;
+    return L;
+}
+
+void list_free( List * L ) {
+    free(L->e);
+    free(L);
+}
+
+void list_insert( List * L, int v ) {
+    if(L->n < L->size) {
+        L->e[L->n] = v;
+        (L->n)++;
+    }
+}
+
+void list_print( List * L ) {
+    int i;
+    printf("[");
+    for(i=0;i<L->n;i++) {
+        printf("%d, ", L->e[i]);
+    }
+    printf("]\n");
+}
+
+void list_expand( List * L, int new_size ) {
+    L->e = realloc( L->e, sizeof(int) * new_size );
+    L->size = new_size;
+}
---
+++ main.c
@@ -0,0 +1,23 @@
+#include "lista.h"
+
+int main() {
+    List * A = list_new(4);
+
+    list_info(A);
+    
+    list_insert(A,4);
+    list_insert(A,10);
+    list_insert(A,2);
+    list_insert(A,7);
+
+    list_info(A);
+    list_expand(A, 8);
+    list_info(A);
+
+    list_insert(A,20);
+
+    list_info(A);
+    
+    list_free(A);
+    return 0;
+}
